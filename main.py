b63aea3f0f03c44bfb916993f9802a215521dc04
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-04-10 09:30:33 +0000

    add text_msg and event_id reaction

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import simplematrixbotlib as botlib
 import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 creds = botlib.Creds(
     "https://matrix.agent.finances.tchap.gouv.fr", 
     os.environ["TCHAP_BOT_SSPHUB_MATRIX_ID"], 
     os.environ["TCHAP_BOT_SSPHUB_PWD"],
-    "session.txt")
+    session_stored_file="session.txt")
 
 config = botlib.Config()
 config.emoji_verify = True
@@ -19,20 +21,25 @@ PREFIX = '!'
 async def echo(room, message):
     match = botlib.MessageMatch(room, message, bot, PREFIX)
 
-    if match.is_not_from_this_bot() and match.prefix() and match.command("echo"):
+    if match.is_not_from_this_bot() and match.command("coucou"):
 
         await bot.api.send_text_message(
-            room.room_id, " ".join(arg for arg in match.args())
+            room.room_id, "Coucou, il est " + datetime.now(ZoneInfo("Europe/Paris")).strftime("%H:%M:%S") + " à Paris."
             )
 
 @bot.listener.on_message_event
 async def example(room, message):
     match = botlib.MessageMatch(room, message, bot)
     example_reaction = "✅"
-    if match.is_not_from_this_bot() and match.prefix() and match.contains(r'\[([^\]]+)\]\(([^)]+)\)'):
+    if match.is_not_from_this_bot() and match.contains('href'):
         await bot.api.send_reaction(
             room_id=room.room_id,
             event=message,
             key=example_reaction)
+
+        await bot.api.send_text_message(
+                room_id=room.room_id,
+                message=f"I reacted to your message {match.event.body}", 
+                reply_to=match.event.event_id)
 
 bot.run()
