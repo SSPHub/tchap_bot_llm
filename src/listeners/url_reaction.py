119d0461b9330ca0db9d4f787e7af5eb7f3a163d
nicotlm <1820-nicolastlm@users.noreply.git.lab.sspcloud.fr>
2026-05-27 13:32:18 +0000

    no url reaction

--- src/listeners/url_reaction.py
+++ src/listeners/url_reaction.py
@@ -1,18 +1,18 @@
-import simplematrixbotlib as botlib
+# import simplematrixbotlib as botlib
 
 
-def register(bot: botlib.Bot, prefix: str) -> None:
-    @bot.listener.on_message_event
-    async def react_to_url(room, message):
-        match = botlib.MessageMatch(room, message, bot)
-        example_reaction = "✅"
-        if match.is_not_from_this_bot() and match.contains("href"):
-            await bot.api.send_reaction(
-                room_id=room.room_id, event=message, key=example_reaction
-            )
+# def register(bot: botlib.Bot, prefix: str) -> None:
+#     @bot.listener.on_message_event
+#     async def react_to_url(room, message):
+#         match = botlib.MessageMatch(room, message, bot)
+#         example_reaction = "✅"
+#         if match.is_not_from_this_bot() and match.contains("href"):
+#             await bot.api.send_reaction(
+#                 room_id=room.room_id, event=message, key=example_reaction
+#             )
 
-            await bot.api.send_text_message(
-                room_id=room.room_id,
-                message=f"I reacted to your message {match.event.body}",
-                reply_to=match.event.event_id,
-            )
+#             await bot.api.send_text_message(
+#                 room_id=room.room_id,
+#                 message=f"I reacted to your message {match.event.body}",
+#                 reply_to=match.event.event_id,
+#             )
