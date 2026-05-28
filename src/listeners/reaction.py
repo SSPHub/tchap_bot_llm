c1ca69f0fe1f6492fe61ac4843ffb235c3113df6
nicotlm <1820-nicolastlm@users.noreply.git.lab.sspcloud.fr>
2026-05-28 11:42:11 +0000

    reaction to proxy

--- src/listeners/reaction.py
+++ src/listeners/reaction.py
@@ -1,12 +1,12 @@
 import simplematrixbotlib as botlib
 
 
-def no_register(bot: botlib.Bot, prefix: str) -> None:
+def register(bot: botlib.Bot, prefix: str) -> None:
     @bot.listener.on_message_event
-    async def react_to_url(room, message):
+    async def react_to_proxy(room, message):
         match = botlib.MessageMatch(room, message, bot)
-        example_reaction = "✅"
-        if match.is_not_from_this_bot() and match.contains("href"):
+        example_reaction = "😖"
+        if match.is_not_from_this_bot() and match.contains("proxy"):
             await bot.api.send_reaction(
                 room_id=room.room_id, event=message, key=example_reaction
             )
